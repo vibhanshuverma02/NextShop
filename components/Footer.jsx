d6e4946cd4c4f5e4abe540182d3af8e993f959d8
vibhanshuverma02 <vibhanshuverma.dpsr@gmail.com>
2025-05-29 17:44:10 +0530

    producation

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -48,8 +48,8 @@ const Footer = () => {
 
       <div className='copyright'>
         <p>Copyright © 2022 Dine Market</p>
-        <p>Design by. <span>Weird Design Studio</span></p>
-        <p>Code by. <span>shabrina12 on github</span></p>
+        <p>Design by. <span>Vibhanshu verma</span></p>
+        <p>Code by. <span>vibhanshuveram02 on github</span></p>
       </div>
     </footer>
   )
